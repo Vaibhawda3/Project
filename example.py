23c5d9d2e4ecfb65a8979d16c161c12d68ff4e64
Vaibhawda3 <53165631+Vaibhawda3@users.noreply.github.com>
2020-03-26 14:30:32 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,18 @@
+import sys
+import mlflow
+import os
+import mlflow.sklearn
+
+if __name__='__main__':
+	a=int(sys.argv[1])
+	b=int(sys.argv[2])
+	mlflow.log_param("val1",a)
+	mlflow.log_param("val2",b)
+	c=a*b
+	print("Multiplication :"+str(c))
+	cwd = os.getcwd()    
+    print(cwd)
+    with open(cwd+'/mlflowfile.txt', 'w') as f:
+        f.write("Fan of Python")
+    mlflow.log_metric("mutiplication",c)
+    mlflow.log_artifact(cwd+'/mlflowfile.txt')
